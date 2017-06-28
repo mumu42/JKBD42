495ce10786f73ac6740d109acc3fccaa505ed3e8
mumu <2227875072@qq.com>
2017-06-28 23:05:41 +0800

    闪存

---
+++ app/src/main/java/com/example/administrator/jkbd/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.administrator.jkbd;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        ct.start();
+    }
+
+    CountDownTimer ct=new CountDownTimer(3000,1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent it=new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(it);
+            SplashActivity.this.finish();
+        }
+    };
+
+}
