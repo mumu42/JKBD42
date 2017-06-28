5a80dbe42e45a812fee90dc70f80da298cc4a55e
mumu <2227875072@qq.com>
2017-06-29 00:37:08 +0800

    实体类

---
+++ app/src/main/java/com/example/administrator/jkbd/Other.java
@@ -0,0 +1,25 @@
+package com.example.administrator.jkbd;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class Other {
+
+    private int error_code;
+    private String reason;
+
+    public int getError_code(){
+        return error_code;
+    }
+    public void setError_code(int error_code){
+        this.error_code=error_code;
+    }
+
+    public String getReason(){
+        return reason;
+    }
+    public void setReason(String reason){
+        this.reason=reason;
+    }
+}
