6b889f21b83b10dbcffa1a74aa2f4df1e314d8b8
mumu <2227875072@qq.com>
2017-07-01 15:37:24 +0800

    ReaultUtils

--- app/src/main/java/com/example/administrator/jkbd/activity/Exam.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/Exam.java
@@ -2,7 +2,9 @@ package com.example.administrator.jkbd.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import com.example.administrator.jkbd.ExamApplication;
 import com.example.administrator.jkbd.R;
 
 /**
@@ -11,9 +13,28 @@ import com.example.administrator.jkbd.R;
 
 public class Exam extends AppCompatActivity {
 
+    TextView tv_exam,tv_question,tv_item1,tv_item2,tv_item3,tv_item4;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exam);
+        init();
+        InitData();
+    }
+
+    private void InitData() {
+        String e=ExamApplication.getInstance().getExamInfos().toString();
+        if(e!=null)
+        tv_exam.setText(e);
+    }
+
+    private void init() {
+        tv_item4=(TextView)findViewById(R.id.tv_item4);
+        tv_item3=(TextView)findViewById(R.id.tv_item3);
+        tv_item2=(TextView)findViewById(R.id.tv_item2);
+        tv_item1=(TextView)findViewById(R.id.tv_item1);
+        tv_question=(TextView)findViewById(R.id.tv_question);
+        tv_exam=(TextView)findViewById(R.id.tv_examinfo);
     }
 
 }
