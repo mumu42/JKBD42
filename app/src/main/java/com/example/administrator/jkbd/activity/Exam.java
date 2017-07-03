8699701d926b5fc2ccd38c54177bafb2576bb782
mumu <2227875072@qq.com>
2017-07-03 11:01:44 +0800

    id

--- app/src/main/java/com/example/administrator/jkbd/activity/Exam.java
+++ app/src/main/java/com/example/administrator/jkbd/activity/Exam.java
@@ -106,7 +106,7 @@ public class Exam extends AppCompatActivity {
     private void showQuestions(List<Question> questions) {
         Question questionlist=questions.get(0);
         if(questionlist!=null){
-//           tv_id.setText(questionlist.getId());
+            tv_id.setText(Integer.toString(questionlist.getId()));
             tv_question.setText(questionlist.getQuestion());
             Picasso.with(Exam.this).load(questionlist.getUrl()).into(image);
             tv_item1.setText(questionlist.getItem1());
